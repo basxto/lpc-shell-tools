f256ebbae8c046b5290b7b0e4c7eb1aa169df64a
Sebastian Riedel <git@basxto.de>
2018-09-28 13:41:19 +0200

    Add script for placing heads and hair

---
+++ duplimap.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# csv cell syntax is: index;xoffset;yoffset
+if [ "$#" -lt 4 ]; then
+    echo "$0 <file.png> <animation.map.csv> <tile width> <tile height>"
+    echo "Example: $0 hair.png animation/walkcycle.map.csv 64 64"
+    echo "This needs imagemagick installed"
+    exit 1
+fi
+dimensions=( $(file $1 | grep -Eo "[[:digit:]]* x [[:digit:]]*") )
+width=${dimensions[0]}
+height=${dimensions[2]}
+row=0
+num=0
+name=$(echo $1 | sed -E "s/\.png//g")
+animation=$(basename $2 | sed -E "s/\.map\.csv//g")
+mkdir -p tmp_${name}
+for line in $(cat $2); do
+    col=0
+    for cell in $(echo ${line} | tr ',' ' '); do
+        array=( $(echo ${cell} | tr ';' ' ') )
+        index=${array[0]}
+        xoffset=${array[1]}
+        yoffset=${array[2]}
+        x=$(( ($3 * $index) % $width))
+        y=$(($4 * (($3 * $index) / $width) ))
+        # cut out
+        echo convert -background none $1 -crop $3x$4+$((x-xoffset))+$((y-yoffset)) tmp_${name}/${num}.png
+        convert -background none $1 -crop $3x$4+$((x-xoffset))+$((y-yoffset)) tmp_${name}/${num}.png
+        # fix size if we lef the area of the image
+        convert -background none -extent $3x$4 tmp_${name}/${num}.png tmp_${name}/${num}.png 
+        col=$((col+1))
+        num=$((num+1))
+    done
+    row=$((row+1))
+done
+montage -background none -tile ${col}x${row} tmp_${name}/*.png ${name}_${animation}.png
